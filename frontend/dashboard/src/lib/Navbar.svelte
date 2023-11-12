37a98ec934b8ad2d38597b42751330114da8dde8
hanidaoud <101226230+hanidaoud@users.noreply.github.com>
2023-11-12 05:51:17 +0100

    front/select

--- frontend/dashboard/src/lib/Navbar.svelte
+++ frontend/dashboard/src/lib/Navbar.svelte
@@ -1,8 +1,9 @@
 <script>
+  export let selection = 0;
   let items = [
     {title: "Dashboard", icon: "⧉"},
     {title: "Setting", icon: "⧉"},
-  ]
+  ];
 </script>
 
 <div class="container">
@@ -11,11 +12,18 @@
     font-bold text-xl mb-10"
   >Ecosync</div>
   <div></div>
-  {#each items as item}
-    <div class="h-16 hover:bg-red-400 grid grid-cols-2 pl-10 pr-24 justify-center items-center text-white font-bold">
-      <span>{item.icon}</span>
-      <span>{item.title}</span>
-    </div>
+  {#each items as item, i}
+    {#if i === selection}
+      <div class="h-16 bg-red-400 grid grid-cols-2 pl-10 pr-24 justify-center items-center text-white font-bold">
+        <span>{item.icon}</span>
+        <span>{item.title}</span>
+      </div>
+    {:else}  
+      <div class="h-16 hover:bg-red-400 grid grid-cols-2 pl-10 pr-24 justify-center items-center text-white font-bold">
+        <span>{item.icon}</span>
+        <span>{item.title}</span>
+      </div>
+    {/if}
   {/each}
 </div>
 
