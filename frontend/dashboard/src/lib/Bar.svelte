02dedf828f576c21f80b0e8348c15449997f9306
hanidaoud <101226230+hanidaoud@users.noreply.github.com>
2023-11-12 05:48:16 +0100

    front/chart

---
+++ frontend/dashboard/src/lib/Bar.svelte
@@ -0,0 +1,10 @@
+<div class="container">
+
+</div>
+
+<style>
+  .container {
+    @apply h-20 border-b-2 border-gray-300;
+    width: calc(100vw - 256px);
+  }
+</style>
---
+++ frontend/dashboard/src/lib/Navbar.svelte
@@ -0,0 +1,26 @@
+<script>
+  let items = [
+    {title: "Dashboard", icon: "⧉"},
+    {title: "Setting", icon: "⧉"},
+  ]
+</script>
+
+<div class="container">
+  <div 
+    class="h-20 border-b-2 border-gray-400 flex justify-center items-center text-white
+    font-bold text-xl mb-10"
+  >Ecosync</div>
+  <div></div>
+  {#each items as item}
+    <div class="h-16 hover:bg-red-400 grid grid-cols-2 pl-10 pr-24 justify-center items-center text-white font-bold">
+      <span>{item.icon}</span>
+      <span>{item.title}</span>
+    </div>
+  {/each}
+</div>
+
+<style>
+  .container {
+    @apply h-screen w-64 bg-gray-900;
+  }
+</style>
